29ea4d61c7183371a5c42c75f8b669fcc2478a8e
FlorM <flowy6290@gmail.com>
2025-02-19 22:00:31 -0700

    Se organizan las colas
    Cuando una caja esta desocupada se ocupa por cierto tiempo y luego se vuelve a desocupar

--- Cola/main.cpp
+++ Cola/main.cpp
@@ -43,27 +43,54 @@ int main()
 //        cout << endl;
         srand (time(NULL));
 
-        Cola<string> ColaTiempo;
-        int tiempo = (rand() % 15 +1);
-        int tiempoQuitar = (rand() % 15 +1);
-        for (int i = 0; i < 120; ++i){
-            if(tiempo == i){
-                ColaTiempo.Encolar("Pedrito");
-                ColaTiempo.imprimir();
-                cout << endl;
-                tiempo = (rand() % 7 +1) +i;
-            }
-            if((ColaTiempo.EstaVacia()) && (tiempoQuitar == i)){
-                tiempoQuitar = (rand() % 15 +1) +i;
-            }else if ((!ColaTiempo.EstaVacia()) && (tiempoQuitar == i)){
-                ColaTiempo.Desencolar();
-                cout << "desencolado    " << endl;
-                ColaTiempo.imprimir();
-                cout << endl;
-                tiempoQuitar = (rand() % 15 +1) +i;
+        Cola<string> Caja1;
+        Cola<string> Caja2;
+        Cola<string> Caja3;
+        Cola<string> Fila;
+        int tiempo = (rand() % 10 +1);
+        int tiempoQuitar1 = (rand() % 15 +1);
+        int tiempoQuitar2 = (rand() % 15 +1);
+        int tiempoQuitar3 = (rand() % 15 +1);
+        int tiempoBanco = (rand() % 120 +120);
 
+        for (int i = 0; i < tiempoBanco; ++i){
+            cout << "Reloj: " << i << endl;
+
+            if(Caja1.EstaVacia()){
+                cout << "La caja 1 esta desocupada" << endl;
+                Caja1.Encolar("Persona");
+                tiempoQuitar1 = (rand() % 15 +1) + i;
+            }else if(tiempoQuitar1 == i){
+                cout << "La caja 1 esta ocupada por " << Caja1.ObtenerPrimero() << endl;
+                Caja1.Desencolar();
+            }else{
+                cout << "La caja 1 esta ocupada por " << Caja1.ObtenerPrimero() << endl;
             }
+
+            if(Caja2.EstaVacia()){
+                cout << "La caja 2 esta desocupada" << endl;
+                Caja2.Encolar("Persona");
+                tiempoQuitar2 = (rand() % 15 +1) + i;
+            }else if(tiempoQuitar2 == i){
+                cout << "La caja 2 esta ocupada por " << Caja2.ObtenerPrimero() << endl;
+                Caja2.Desencolar();
+            }else{
+                cout << "La caja 2 esta ocupada por " << Caja2.ObtenerPrimero() << endl;
+            }
+
+            if(Caja3.EstaVacia()){
+                cout << "La caja 3 esta desocupada" << endl;
+                Caja3.Encolar("Persona");
+                tiempoQuitar3 = (rand() % 15 +1) + i;
+            }else if(tiempoQuitar3 == i){
+                cout << "La caja 3 esta ocupada por " << Caja3.ObtenerPrimero() << endl;
+                Caja3.Desencolar();
+            }else{
+                cout << "La caja 3 esta ocupada por " << Caja3.ObtenerPrimero() << endl;
+            }
+
             Sleep(1000);
+            system("cls");
         }
 
     }catch(const char *mensaje){
