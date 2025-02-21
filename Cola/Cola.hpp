d195a2bf678cc0f5a6502f2484373fd5ff19ae95
FlorM <flowy6290@gmail.com>
2025-02-20 21:42:13 -0700

    Cambios varios
    Se añaden los tiempos de atencion y se cambia el registro de los clientes atendidos, se crea la bitacora

--- Cola/Cola.hpp
+++ Cola/Cola.hpp
@@ -4,28 +4,83 @@ template <typename Tipo>
 class Cola{
 public:
     //Constructor por default
+    /** \brief Constructor de la clase
+     */
     Cola();
     //Constructor de copias
+    /** \brief Constructor de la clase para copiar otros objetos de la clase
+     *
+     * \param c const Cola&
+     *
+     */
     Cola(const Cola &c);
     //Operador =
+    /** \brief M&eacute;todo del operador = para copiar objetos de la clase a otros objetos
+     *
+     * \param const Cola &v, el objeto de la clase que va a copiar
+     */
     Cola & operator= (const Cola &v);
     //Destructor
+    /** \brief Destructor de la clase
+     *
+     *
+     */
     ~Cola();
     //Encolar
+    /** \brief M&eacute;todo para a&ntilde; adir cosas de Tipo dentro de la cola
+     *
+     * \param valor Tipo, el valor que se va a a&ntilde; adir a la cola
+     * \return void
+     *
+     */
     void Encolar(Tipo valor);
     //Desencolar
+    /** \brief M&eacute;todo para desencolar el primero valor de la cola
+     *
+     * \return void
+     *
+     */
     void Desencolar();
-    //Conocer primero de la cola
+    //Conocer primer valor de la cola
+    /** \brief M&eacute;todo para obtener el primer valor de la cola
+     *
+     * \return Tipo, el valor que se quiere obtener
+     *
+     */
     Tipo ObtenerPrimero();
     //Conocer el ultimo de la cola
-    Tipo ObtenerUltimo();
+    /** \brief M&eacute;todo para obtener el ultimo valor de la cola
+     *
+     * \return Tipo, el valor que se quiere obtener
+     *
+     */  Tipo ObtenerUltimo();
     //Conocer el numero de elementos
+    /** \brief M&eacute;todo para conocer la cantidad de elementos que tiene la cola
+     *
+     * \return int cantidad de elementos de la cola
+     *
+     */
     int CantidadElementos();
-    //Conocer si está vacía
+    //Conocer si est&aacute; vac&iacute;a
+    /** \brief M&eacute;todo para conocer si la cola esta vac&iacute;a o no
+     *
+     * \return bool
+     *
+     */
     bool EstaVacia() const;
     //Vaciar
+    /** \brief M&eacute;todo que vac&iacute;a la cola
+     *
+     * \return void
+     *
+     */
     void Vaciar();
     //imprimir
+    /** \brief M&eacute;todo que imprime los valores de la cola
+     *
+     * \return void
+     *
+     */
     void imprimir() const;
 private:
     int numElem;
--- Cola/main.cpp
+++ Cola/main.cpp
@@ -8,6 +8,128 @@ using namespace std;
 
 const int MAX_CLI = 100;
 
+void leerNombres(const string& nombreArchivo, string nombrecli[], int& totalcli);
+int main()
+{
+    try{
+
+        srand (time(NULL));
+
+        Cola<string> Caja1, Caja2, Caja3, Fila, Atendidos1, Atendidos2, Atendidos3;
+
+        int tiempo = (rand() % 5 +1);
+        int tiempoQuitar1 = (rand() % 15 +1);
+        int tiempoQuitar2 = (rand() % 15 +1);
+        int tiempoQuitar3 = (rand() % 15 +1);
+        int tiempoBanco = (rand() % 120 +120);
+        string nombreArchivo = "Nombres.txt";
+        string nombrecli[MAX_CLI];
+        int totalcli = 0;
+        int indice = 0;
+        int aux1, aux2, aux3; //miden el tiempo de atencion por cada cliente
+        leerNombres(nombreArchivo, nombrecli, totalcli);
+
+        cout << "Las cajas atender\240n a tantos clientes puedan en " << tiempoBanco << " minutos" << endl;
+        cout << "\nEl tiempo que tarda cada uno de las personas es totalmente aleatorio" << endl;
+
+        system("pause");
+
+
+        for (int i = 0; i < tiempoBanco; ++i){
+            cout << "Las cajas atender\240n a tantos clientes puedan en " << tiempoBanco << " minutos" << endl;
+            cout << "\nReloj: " << i << endl;
+
+            if ( (indice < totalcli) && ( tiempo == i) ){
+                Fila.Encolar(nombrecli[indice++]);
+                tiempo = (rand() % 5 +1) +i;
+            }
+
+            if(Caja1.EstaVacia() && !Fila.EstaVacia()){
+                string cliente = Fila.ObtenerPrimero();
+                Fila.Desencolar();
+                Caja1.Encolar(cliente);
+                Atendidos1.Encolar(cliente);
+                tiempoQuitar1 = (rand() % 10 + 5) + i;
+            }else if(tiempoQuitar1 == i && !Caja1.EstaVacia()){
+                Caja1.Desencolar();
+            }
+
+            cout << "Caja 1: ";
+            if (!Caja1.EstaVacia()){
+                cout << Caja1.ObtenerPrimero();
+                cout << "\t Tiempo de atenci\242n: " << aux1;
+                ++aux1;
+            } else {
+                cout << "Desocupada";
+                aux1 = 0;
+            }
+            cout << endl;
+
+            if(Caja2.EstaVacia() && !Fila.EstaVacia()){
+                string cliente = Fila.ObtenerPrimero();
+                Fila.Desencolar();
+                Caja2.Encolar(cliente);
+                Atendidos2.Encolar(cliente);
+                tiempoQuitar2 = (rand() % 10 + 5) + i;
+            }else if(tiempoQuitar2 == i && !Caja2.EstaVacia()){
+                Caja2.Desencolar();
+            }
+
+            cout << "Caja 2: ";
+            if (!Caja2.EstaVacia()){
+                cout << Caja2.ObtenerPrimero();
+                cout << "\t Tiempo de atenci\242n: " << aux2;
+                ++aux2;
+            } else {
+                cout << "Desocupada";
+                aux2 = 0;
+            }
+            cout << endl;
+
+            if(Caja3.EstaVacia() && !Fila.EstaVacia()){
+                string cliente = Fila.ObtenerPrimero();
+                Fila.Desencolar();
+                Caja3.Encolar(cliente);
+                Atendidos3.Encolar(cliente);
+                tiempoQuitar3 = (rand() % 10 + 5) + i;
+            }else if(tiempoQuitar3 == i && !Caja3.EstaVacia()){
+                Caja3.Desencolar();
+            }
+
+            cout << "Caja 3: ";
+            if (!Caja3.EstaVacia()){
+                cout << Caja3.ObtenerPrimero();
+                cout << "\t Tiempo de atenci\242n: " << aux3;
+                ++aux3;
+            } else {
+                cout << "Desocupada";
+                aux3 = 0;
+            }
+
+            cout << endl;
+
+            cout << "Clientes en espera:\n";
+            Fila.imprimir();
+
+            Sleep(300);
+            system("cls");
+        }
+
+        cout << "Clientes atendidos por la Caja 1: " << endl;
+        Atendidos1.imprimir();
+        cout << endl << "Clientes atendidos por la Caja 2: " <<endl;
+        Atendidos2.imprimir();
+        cout << endl << "Clientes atendidos por la Caja 3: " << endl;
+        Atendidos3.imprimir();
+        cout << endl << "Clientes que no fueron atendidos:" << endl;
+        Fila.imprimir();
+
+    }catch(const char *mensaje){
+        cerr << mensaje;
+    }
+    return 0;
+}
+
 void leerNombres(const string& nombreArchivo, string nombrecli[], int& totalcli){
     ifstream archivo(nombreArchivo);
     if (archivo.is_open()){
@@ -18,146 +140,4 @@ void leerNombres(const string& nombreArchivo, string nombrecli[], int& totalcli)
             }
         } archivo.close();
     }
-}
-
-int main()
-{
-    try{
-//        Cola <double> ColaDouble;
-//        ColaDouble.Encolar(12);
-//        ColaDouble.Encolar(3.2);
-//        ColaDouble.Encolar(7);
-//        ColaDouble.imprimir();
-//        cout << endl;
-//        ColaDouble.Desencolar();
-//        ColaDouble.imprimir();
-//        ColaDouble.Desencolar();
-//        ColaDouble.imprimir();
-//        cout << endl << ColaDouble.CantidadElementos() << endl;
-//        ColaDouble.Encolar(12);
-//
-//        ColaDouble.imprimir();
-//        cout << endl;
-//        ColaDouble.Vaciar();
-//        ColaDouble.imprimir();
-//        cout << endl;
-//
-//        ColaDouble.Encolar(12);
-//        ColaDouble.Encolar(3);
-//        ColaDouble.Encolar(-2.3);
-//        ColaDouble.imprimir();
-//        cout << endl;
-//
-//        Cola<double> ColaCopia(ColaDouble);
-//        ColaDouble.Desencolar();
-//        cout << endl;
-//        ColaDouble.imprimir();
-//        cout << endl;
-//        ColaCopia.imprimir();
-//
-//        cout << endl;
-        
-        srand (time(NULL));
-
-        Cola<string> Caja1;
-        Cola<string> Caja2;
-        Cola<string> Caja3;
-        Cola<string> Fila;
-        int tiempo = (rand() % 10 +1);
-        int tiempoQuitar1 = (rand() % 15 +1);
-        int tiempoQuitar2 = (rand() % 15 +1);
-        int tiempoQuitar3 = (rand() % 15 +1);
-        int tiempoBanco = (rand() % 120 +120);
-        string nombreArchivo = "Nombres.txt";
-        string nombrecli[MAX_CLI];
-        int totalcli = 0;
-        int indice = 0;
-        leerNombres(nombreArchivo, nombrecli, totalcli);
-
-        cout << "Las cajas atenderan a tantos clientes puedan en " << tiempoBanco << " minutos" << endl;
-        cout << "\nEl tiempo que tarda cada uno de las personas es totalmente aleatorio" << endl;
-
-        system("pause");
-
-
-        for (int i = 0; i < tiempoBanco; ++i){
-            cout << "Las cajas atenderan a tantos clientes puedan en " << tiempoBanco << " minutos" << endl;
-            cout << "\nReloj: " << i << endl;
-
-            if (indice < totalcli && rand() % 5 == 0){
-                Fila.Encolar(nombrecli[indice++]);
-            }
-
-            if(Caja1.EstaVacia() && !Fila.EstaVacia()){
-                string cliente = Fila.ObtenerPrimero();
-                Fila.Desencolar();
-                Caja1.Encolar(cliente);
-                tiempoQuitar1 = (rand() % 15 +1) + i;
-            }else if(tiempoQuitar1 == i && !Caja1.EstaVacia()){
-                Caja1.Desencolar();
-            }
-
-            cout << "Caja 1: ";
-            if (!Caja1.EstaVacia()){
-                cout << Caja1.ObtenerPrimero();
-            } else {
-                cout << "Desocupada";
-            }
-            cout << endl;
-
-            if(Caja2.EstaVacia() && !Fila.EstaVacia()){
-                string cliente = Fila.ObtenerPrimero();
-                Fila.Desencolar();
-                Caja2.Encolar(cliente);
-                tiempoQuitar2 = (rand() % 15 +1) + i;
-            }else if(tiempoQuitar2 == i && !Caja1.EstaVacia()){
-                Caja2.Desencolar();
-            }
-            
-            cout << "Caja 2: ";
-            if (!Caja2.EstaVacia()){
-                cout << Caja2.ObtenerPrimero();
-            } else {
-                cout << "Desocupada";
-            }
-            cout << endl;
-
-            if(Caja3.EstaVacia() && !Fila.EstaVacia()){
-                string cliente = Fila.ObtenerPrimero();
-                Fila.Desencolar();
-                Caja3.Encolar(cliente);
-                tiempoQuitar3 = (rand() % 15 +1) + i;
-            }else if(tiempoQuitar3 == i && !Caja1.EstaVacia()){
-                Caja3.Desencolar();
-            }
-
-            cout << "Caja 3: ";
-            if (!Caja3.EstaVacia()){
-                cout << Caja3.ObtenerPrimero();
-            } else {
-                cout << "Desocupada";
-            }
-
-            cout << endl;
-
-            cout << "Clientes en espera:\n";
-            Fila.imprimir();
-
-            Sleep(1000);
-            system("cls");
-        }
-
-        cout << "Clientes atendidos por la Caja 1: ";
-        Caja1.imprimir();
-        cout << "Clientes atendidos por la Caja 2: ";
-        Caja2.imprimir(); 
-        cout << "Clientes atendidos por la Caja 3: ";
-        Caja3.imprimir();
-        cout << "Clientes que no fueron atendidos:";
-        Fila.imprimir();
-
-    }catch(const char *mensaje){
-        cerr << mensaje;
-    }
-    return 0;
 }
